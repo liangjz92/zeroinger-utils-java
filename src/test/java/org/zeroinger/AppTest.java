fe966b5fc70192616c22477d7651b97471d0d482
梁建增 <jiangzeng.ljz@alibaba-inc.com>
2019-10-28 20:53:27 +0800

    create empty project

---
+++ src/test/java/org/zeroinger/AppTest.java
@@ -0,0 +1,20 @@
+package org.zeroinger;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
